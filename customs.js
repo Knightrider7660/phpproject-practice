10125177de0191758438fa276a70b9840e237681
Sukanta Purkayastha <30552975+Knightrider7660@users.noreply.github.com>
2018-05-01 10:55:23 +0600

    first commit

---
+++ customs.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 50) {
+        $('.menu').addClass('fixed');
+    } else {
+        $('.menu').removeClass('fixed');
+    }
+});
+
+});
